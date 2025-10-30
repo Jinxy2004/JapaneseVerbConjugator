695486f1e3251898367f5b45b373468aeb446efd
Jinxy2004 <jinxy2006@yahoo.com>
2025-10-30 18:21:44 -0400

    Platform transfer?

--- MainConjugator.cpp
+++ MainConjugator.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
 #include <locale>
+#include <unordered_map>
+#include <vector>
+#ifdef _WIN32
+#include <io.h>
+#include <fcntl.h>
+#endif
 using namespace std;
 
 enum VerbType
@@ -43,8 +49,12 @@ void InfinitiveVerbIntoStem(wstring *verb, VerbType type)
 int main()
 {
     wstring verb = L"行く";
+#ifdef _WIN32
+    _setmode(_fileno(stdout), _O_U16TEXT);
+#else
     locale::global(locale("en_US.UTF-8"));
     wcout.imbue(locale());
+#endif
     // wcout << &verb;
     InfinitiveVerbIntoStem(&verb, ichidan);
     // wcout << verb << endl;
