0fa706488f2e0429a6548ee8997c7d84d293e05d
Gerard Hill <jinxy2006@yahoo.com>
2025-10-30 18:10:08 -0400

    Basic starting point.

--- MainConjugator.cpp
+++ MainConjugator.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <string>
+#include <locale>
 using namespace std;
+
+enum VerbType
+{
+    ichidan,
+    godan,
+};
+
+const unordered_map<wstring, vector<wstring>> HIRAGANA_MAP = {
+    {L"う", {L"あ", L"と"}}};
+
+void InfinitiveVerbIntoStem(wstring *verb, VerbType type)
+{
+    if (type == ichidan)
+    {
+        auto it = HIRAGANA_MAP.find(L"う");
+        if (it != HIRAGANA_MAP.end())
+        {
+            for (const auto &val : it->second)
+            {
+                wcout << val << L" ";
+            }
+        }
+        else
+        {
+            wcout << L"Key not found";
+        }
+    }
+    // wcout << L"First character: " << (*verb)[0] << endl;
+    // if (type == special)
+    // {
+    //     if ((*verb)[0] == L'行' && (*verb)[1] != L'う')
+    //     {
+    //         (*verb)[1] = L'き';
+    //     }
+    //     else if ((*verb)[] == L'する')
+    // }
+}
 
 int main()
 {
-    cout << "hello";
+    wstring verb = L"行く";
+    locale::global(locale("en_US.UTF-8"));
+    wcout.imbue(locale());
+    // wcout << &verb;
+    InfinitiveVerbIntoStem(&verb, ichidan);
+    // wcout << verb << endl;
 }
